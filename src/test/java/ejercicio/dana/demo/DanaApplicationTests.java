548fdf9b8d9acd1ab6bb183e96030993618f44e0
laberintolab <daniela@gatillo.cl>
2022-05-01 15:07:18 -0400

    primera version

---
+++ src/test/java/ejercicio/dana/demo/DanaApplicationTests.java
@@ -0,0 +1,13 @@
+package ejercicio.dana.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
